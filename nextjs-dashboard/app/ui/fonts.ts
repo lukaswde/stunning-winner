cdad213f379f8481c8e55b8278f0c6800aa061ef
lukaswde <mail@lukasw.de>
2024-11-10 00:51:17 +0100

    Chapter 3

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { subscribe } from 'diagnostics_channel';
+import { Inter } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Inter({ 
+    weight: ['400', '700'], 
+    subsets: ['latin']
+});
