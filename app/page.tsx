77fb4fe57f061c6ba3a3bc1234c8837f8eef4995
lukaswde <mail@lukasw.de>
2024-11-10 04:35:21 +0100

    Test

--- app/page.tsx
+++ app/page.tsx
@@ -27,6 +27,7 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
           <Link
+            //TODO Test change - remove this!
             href="/dashboard" //TODO drop this it's just for testing
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
