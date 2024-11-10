4b3399839d13a93243f0452c549f45632e387d4a
lukaswde <mail@lukasw.de>
2024-11-10 03:25:22 +0100

    config

---
+++ nextjs-dashboard/.next/server/chunks/ssr/app_2f8a55._.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+"[project]/app/ui/dashboard/nav-links.tsx [app-ssr] (ecmascript)": (() => {{
+
+throw new Error("An error occurred while generating the chunk item [project]/app/ui/dashboard/nav-links.tsx [app-ssr] (ecmascript)\n\nCaused by:\n- failed to analyse ecmascript module '[project]/app/ui/dashboard/nav-links.tsx [app-ssr] (ecmascript)'\n- Cell doesn't exist\n\nDebug info:\n- An error occurred while generating the chunk item [project]/app/ui/dashboard/nav-links.tsx [app-ssr] (ecmascript)\n- Execution of *EcmascriptChunkItemContent::module_factory failed\n- Execution of *EcmascriptChunkItemContent::new failed\n- Execution of <EcmascriptModuleAsset as EcmascriptAnalyzable>::module_content failed\n- Execution of analyse_ecmascript_module failed\n- failed to analyse ecmascript module '[project]/app/ui/dashboard/nav-links.tsx [app-ssr] (ecmascript)'\n- Cell doesn't exist");
+
+}}),
+"[project]/app/dashboard/layout.tsx [app-rsc] (ecmascript, Next.js server component, client modules ssr)": ((__turbopack_context__) => {
+
+var { r: __turbopack_require__, f: __turbopack_module_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, M: __turbopack_modules__, l: __turbopack_load__, j: __turbopack_dynamic__, P: __turbopack_resolve_absolute_path__, U: __turbopack_relative_url__, R: __turbopack_resolve_module_id_path__, b: __turbopack_worker_blob_url__, g: global, __dirname, t: require } = __turbopack_context__;
+{
+}}),
+
+};
